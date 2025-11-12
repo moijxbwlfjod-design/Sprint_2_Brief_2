87ef15c1ebb1abdb4a6cd5a8d3af1fbf732824d3
moijxbwlfjod-design <moijxbwlfjod-design@gmail.com>
2025-11-12 09:57:56 +0100

    feat: l'ajout de validation des formules

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    let curenTab = 8;
+    let curenTab = 1;
     const steps = document.querySelectorAll(".progress-list li");
+    const icons = document.querySelectorAll(".progress-list li svg");
     const progress = document.querySelector(".progress");
     const profileBtn = document.getElementById("profileBtn");
     const profileInput = document.getElementById("profileInput");
@@ -13,8 +14,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const previous_btn = document.getElementById("previous");;
     const next_btn = document.getElementById("next");
     const addLangBtn = document.getElementById("languages_containt_add_btn");
+    const rmLanBtn = document.getElementById("languages_containt_rm_btn");
     const lanNameInput = document.getElementsByClassName("language_name");
     const lanLevelInput = document.getElementsByClassName("language_level");
+    const lanContaint = document.getElementsByClassName("languages_containt_div");
     const lanDiv = document.getElementById("languages_containt");
     const hobbiesInput = document.getElementById("hobbies");
     const hobbiesBtn = document.getElementById("hobbies_btn");
@@ -29,7 +32,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const certificatsDiv = document.getElementById("certificats_popup");
     let certificatsArr = [];
     let profissionalExArr = [];
-
+    const Inputs = document.getElementsByTagName("input");
+    const spanOne = document.querySelector('.cart span');
+    const spanTwo = document.querySelector(".email span");
+    const spanThree = document.querySelector(".linkedin_account span");
+    const spanFour = document.querySelector(".github_account span");
+    let patterns = {
+        names : /^[a-zA-Z ]{2,30}$/,
+        cart_nationale : /^[A-Za-z]{1,2}[0-9]{5,6}$/,
+        email : /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    }
 
 
     function reload_progress_bar(num){
@@ -39,19 +51,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
         
         steps.forEach((step, index) => {
             if(index == num - 1){
-                step.style.backgroundColor = "red";
-                step.style.border = "2px solid white";
+                step.style.backgroundColor = "#007BFF";
+                step.style.boxShadow = "1px 1px 10px #007BFF, -1px -1px 10px #007BFF"
+                icons[index].setAttribute("fill", "white");
+
             } else if ( index < num - 1){
-                step.style.backgroundColor = "red";
+                step.style.backgroundColor = "#007BFF";
                 step.style.border = "0px";
+                step.style.boxShadow = "none";
+                icons[index].setAttribute("fill", "white");
             } else {
-                step.style.backgroundColor = "white";
+                step.style.backgroundColor = "#CED4DA";
                 step.style.border = "0px";
+                step.style.boxShadow = "none";
+                icons[index].setAttribute("fill", "black");
             }
         })
     };
 
-    function clickIcon(num){
+    function specificInputVerification(){
         
     }
 
@@ -129,15 +147,74 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
     };
 
-    
+    function checkInputs(input, expression){
+        return expression.test(input);
+    }
+
+    function checkInputsLength(num){
+        
+        let curentFormInputs = formsContent[num].getElementsByTagName("input");
+    }
 
     next_btn.addEventListener("click", (e)=>{
         e.preventDefault();
-        if(curenTab >= 1 && curenTab < 8){
+        let isValid = true;
+        if(curenTab == 1){
+            if(checkInputs(document.getElementById("cart").value, patterns.cart_nationale)){
+                spanOne.textContent = '';
+                isValid = true;
+            } else {
+                spanOne.textContent = "Please give a valid input";
+                spanOne.style.color = "red";
+                isValid = false;
+            }
+        } else if (curenTab == 2){
+            if(!checkInputs(document.getElementById("email").value, patterns.email)){
+                spanTwo.textContent = "Please give a valid input";
+                spanTwo.style.color = "red";
+                isValid = false;
+            } else {
+                spanTwo.textContent = "";
+                isValid = true;
+            }
+            if(!document.getElementById("linkedin_account").value.includes("linkedin.com/in") && !document.getElementById("linkedin_account").value.includes("https://")){
+                spanThree.textContent = "Please give a valid input";
+                spanThree.style.color = "red";
+                isValid = false;
+            } else {
+                spanThree.textContent = "";
+                isValid = true;
+            }
+            if(!document.getElementById("github_account").value.includes("https://") && !document.getElementById("github_account").value.includes("github.com")){
+                spanFour.textContent = "Please give a valid input";
+                spanFour.style.color = "red";
+                isValid = false;
+            } else {
+                spanFour.textContent = "";
+                isValid = true;
+            }
+        } 
+        if(isValid==true && curenTab >= 1 && curenTab < 8){
             curenTab++;
             reload_progress_bar(curenTab);
             checkCurentForm(curenTab);
         }
+        // for(let i = 1; i < Inputs.length; i++){
+        //     let id = Inputs[i].getAttribute("id");
+        //     let span = document.querySelector(`#${id} + span`);
+        //     if(Inputs[i].value.length == 0){
+        //         span.textContent = "Please give a valid input";
+        //         span.style.color = "red";
+        //     } else {
+        //         span.textContent = ""
+        //     }
+        // }
+
+        // if(curenTab >= 1 && curenTab < 8){
+        //     curenTab++;
+        //     reload_progress_bar(curenTab);
+        //     checkCurentForm(curenTab);
+        // }
     });
 
 
@@ -166,21 +243,30 @@ document.addEventListener("DOMContentLoaded", ()=>{
         reader.readAsDataURL(file);
     });
 
+    rmLanBtn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        if(lanContaint.length > 1){
+            let last_lan = lanContaint.length - 1;
+            lanContaint[last_lan].remove();
+        }
+    })
+
     addLangBtn.addEventListener("click", (e)=>{
         e.preventDefault();
-        lanDiv.innerHTML += `<div class="languages_containt_div">
+        let = curent_lan = lanNameInput.length - 1;
+        if(lanNameInput[curent_lan].value.length > 1 && lanLevelInput[curent_lan].value.length > 1){
+            lanDiv.innerHTML += `<div class="languages_containt_div">
                                 <div class="languages_containt_one flex flex-col mb-[10px]">
                                 <label for="language_name">Language Name:</label>
-                                <input type="text" placeholder="Frensh" class="language_name bg-[white] rounded-[8px]">
+                                <input type="text" placeholder="Frensh" page="5" tp="1" id="language_name" class="bg-[white] rounded-[8px] border border-gray-300 focus:outline-none focus:border-blue-500">
                                 </div>
 
                                 <div class="languages_containt_two flex flex-col mb-[10px]">
                                 <label for="language_level">Your Language Level:</label>
-                                <input type="text" placeholder="B2" class="language_level bg-[white] rounded-[8px]">
+                                <input type="text" placeholder="B2" page="5" tp="1" id="language_level" class="bg-[white] rounded-[8px] border border-gray-300 focus:outline-none focus:border-blue-500">
                             </div>
                             </div>`;
-        
-        
+        }
     })
 
     reload_progress_bar(curenTab);
